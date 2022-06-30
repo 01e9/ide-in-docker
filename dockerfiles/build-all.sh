5d892a36ab009776856019ef647d8cb776da07b8
01e9 <1@01eg.art>
2022-06-30 19:58:22 +0300

    dockerfiles/build-all.sh: added env PUSH

--- dockerfiles/build-all.sh
+++ dockerfiles/build-all.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-IMAGE=01e9/ide-in-docker
-
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 cd "$SCRIPT_DIR"
 
+IMAGE=01e9/ide-in-docker
+PUSH=${PUSH:-}
+
 docker build -t "${IMAGE}" . # base
+[ -n "$PUSH" ] && docker push "${IMAGE}"
 
 for TAG in 'php-js' 'cpp' 'cpp-gpu'
 do
     docker build -t "${IMAGE}:${TAG}" -f "${TAG}.Dockerfile" .
+    [ -n "$PUSH" ] && docker push "${IMAGE}:${TAG}"
 done
